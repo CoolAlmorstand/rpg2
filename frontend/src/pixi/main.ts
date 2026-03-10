a738bc49da631cda8a2e20389e8471d122716d4e
napoleon <loinoobgaming33373@gmail.com>
2026-03-10 23:08:02 +0800

    quick deployment patch2

--- frontend/src/pixi/main.ts
+++ frontend/src/pixi/main.ts
@@ -1,40 +1,40 @@
 
-import "@pixi/layout"
-import { Application, Assets } from 'pixi.js'
-// import { Game } from './game/game'
-import { DisplayManager } from './display-manager/display-manger'
-import { MainMenuSceen } from './screens/main-menu/main-menu'
-import type { IScreen } from './interfaces/IScreen'
+// import "@pixi/layout"
+// import { Application, Assets } from 'pixi.js'
+// // import { Game } from './game/game'
+// import { DisplayManager } from './display-manager/display-manger'
+// import { MainMenuSceen } from './screens/main-menu/main-menu'
+// import type { IScreen } from './interfaces/IScreen'
 
-async function createApp() {
-  const appContainer: HTMLElement = document.getElementById("app")!
-  const app = new Application()
-  await app.init({
-    resizeTo: window,
-    backgroundColor: "#fdf8d4"
-  })
-
-  const displayManger = new DisplayManager()
-
-  app.stage.layout = {
-    width: app.screen.width,
-    height: app.screen.height,
-    justifyContent: "center",
-    alignItems: "center"
-  }
-  
-  await addScreens(displayManger) 
-  app.stage.addChild(displayManger.displayContainer)
-  appContainer.appendChild(app.canvas)
-
-}
-
-
-async function addScreens(displayManger: DisplayManager) {
-  const mainMenuScreen: IScreen = new MainMenuSceen()
-  displayManger.addScreen("main-menu", mainMenuScreen.displayContainer)
-  displayManger.showScreen("main-menu") 
-}
-
-
-createApp()
+// async function createApp() {
+//   const appContainer: HTMLElement = document.getElementById("app")!
+//   const app = new Application()
+//   await app.init({
+//     resizeTo: window,
+//     backgroundColor: "#fdf8d4"
+//   })
+//
+//   const displayManger = new DisplayManager()
+//
+//   app.stage.layout = {
+//     width: app.screen.width,
+//     height: app.screen.height,
+//     justifyContent: "center",
+//     alignItems: "center"
+//   }
+//
+//   await addScreens(displayManger) 
+//   app.stage.addChild(displayManger.displayContainer)
+//   appContainer.appendChild(app.canvas)
+//
+// }
+//
+//
+// async function addScreens(displayManger: DisplayManager) {
+//   const mainMenuScreen: IScreen = new MainMenuSceen()
+//   displayManger.addScreen("main-menu", mainMenuScreen.displayContainer)
+//   displayManger.showScreen("main-menu") 
+// }
+//
+//
+// createApp()
